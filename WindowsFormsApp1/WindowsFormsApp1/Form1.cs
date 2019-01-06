7bd190f9fe8a5d4c7094670f572981504a9a0134
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2019-01-07 03:37:52 +0600

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=WIN-0IQ12IEBH08;Initial Catalog=Sample;Integrated Security=SSPI;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from clients", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from employees", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("Select * from sales", connection);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                this.dataGridView1.DataSource = dataTable;
+            }
+        }
+    }
+}
