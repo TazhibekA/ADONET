560d538446bdf324dfa0bf3bec24d3f330481bb5
TazhibekA <40587394+TazhibekA@users.noreply.github.com>
2019-01-15 18:50:54 +0600

    Add files via upload

---
+++ TwoMigrations/TwoMigrations/Migrations/201901151245539_init.cs
@@ -0,0 +1,26 @@
+namespace TwoMigrations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class init : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Login = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
